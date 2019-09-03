a5d291923a046feee1f0954875c18f2f58219fb4
marie-prog0627 <marie.726project@gmail.com>
2019-09-03 14:18:32 +0900

    initial commit

---
+++ Unitychan_desktop/Assets/Added/Added Script/NativePluginLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class NativePluginLoader : MonoBehaviour
+{
+    [DllImport("UnityTransparentizeBackgroundPlugin")]
+    private static extern void TransparentizeBackground();
+
+    // Use this for overall initialization on load
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        // Invoke transparent script when it's not on editor
+        #if UNITY_EDITOR
+        // None
+        #else
+        TransparentizeBackground();
+        #endif
+    }
+
+}
