561187a81c410a8f2091b4299819111af4edad6a
marie-prog0627 <marie.726project@gmail.com>
2019-09-04 12:45:17 +0900

    Implementation of Voice Recognization

---
+++ Unitychan_desktop/Assets/Added/Added Script/AssistantBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssistantBehaviour: MonoBehaviour {
+
+    private Animator animator;
+    private int ActionNum = 0;
+
+    // Inspector
+    [SerializeField] private WSVoiceRecognition Recognition;
+
+    int hashPhase = Animator.StringToHash("Phase");
+
+    void Start(){
+        animator = GetComponent<Animator>();
+        Recognition = GetComponent<WSVoiceRecognition>();
+    }
+    
+    void Update(){
+        var state = animator.GetCurrentAnimatorStateInfo(0);
+
+        animator.SetInteger("Phase", Action(Recognition.Key));
+        Debug.Log("Assist Judge");
+        Debug.Log(Recognition.Key);
+
+        Debug.Log(state.IsName("Base Layer.wait." + Recognition.Key));
+
+        if(state.IsName("Base Layer.wait." + Recognition.Key)){
+            Recognition.Key = "";
+        }
+
+    }
+
+    int Action(string key){
+        int num = 0;
+        switch(key){
+            case "こんにちは":
+                num = 2;
+                break;
+            case "さようなら":
+                num = 3;
+                break;
+            default:
+                break;
+        }
+        return num;
+    }
+
+    /* override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        Debug.Log("Assist Judge");
+        Debug.Log(Recognition.KeyNum);
+
+        animator.SetInteger(hashRandom,  Recognition.KeyNum);
+
+        Recognition.KeyNum = Random.Range(0,1);
+    } */
+}
