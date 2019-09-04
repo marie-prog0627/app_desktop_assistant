87d9a083b5dec8ba9e31e9bb37a2c245d21aec65
marie-prog0627 <marie.726project@gmail.com>
2019-09-04 19:11:47 +0900

    Move temporarily

--- Unitychan_desktop/Assets/Added/Added Script/AssistantBehaviour.cs
+++ Unitychan_desktop/Assets/Added/Added Script/AssistantBehaviour.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 
 public class AssistantBehaviour: MonoBehaviour {
 
     private Animator animator;
     private int ActionNum = 0;
+
+    // メンバ変数
+    Process proc;
 
     // Inspector
     [SerializeField] private WSVoiceRecognition Recognition;
@@ -21,10 +25,10 @@ public class AssistantBehaviour: MonoBehaviour {
         var state = animator.GetCurrentAnimatorStateInfo(0);
 
         animator.SetInteger("State", Action(Recognition.Key));
-        Debug.Log("Assist Judge");
-        Debug.Log(Recognition.Key);
+        UnityEngine.Debug.Log("Assist Judge");
+        UnityEngine.Debug.Log(Recognition.Key);
 
-        Debug.Log(state.IsName("Base Layer.wait." + Recognition.Key));
+        UnityEngine.Debug.Log(state.IsName("Base Layer.wait." + Recognition.Key));
 
         if(state.IsName("Base Layer.wait." + Recognition.Key)){
             Recognition.Key = "";
@@ -41,13 +45,36 @@ public class AssistantBehaviour: MonoBehaviour {
             case "さようなら":
                 num = 3;
                 break;
-            case "開いて":
+            case "PowerPoint 開いて":
                 num = 4;
+                OpenApp("PowerPoint");
                 break;
             default:
                 break;
         }
         return num;
+    }
+
+    void OpenApp(string AppName){
+        // 別アプリ(プロセス)起動
+        proc = new Process();
+        proc.StartInfo.FileName = "/Applications/Microsoft PowerPoint.app.installBackup";   // 起動させる別アプリ名をここに入れて下さい(フルパス指定でも可) 
+        proc.Start();
+    }
+
+    // アプリ終了時に呼ばれる
+    private void OnApplicationQuit()
+    {
+        // 別アプリ終了処理
+
+        if (!proc.HasExited)
+        {
+            // 別アプリが起動中の場合のみ終了させる
+            proc.CloseMainWindow();
+        }
+
+        proc.Close();
+        proc = null;
     }
 
     /* override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
